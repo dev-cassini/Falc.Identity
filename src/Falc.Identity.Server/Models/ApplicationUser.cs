cbd15d54b04002da47843f6005c37b29918aac11
dev-cassini <ryan.dremza@gmail.com>
2024-08-25 12:18:05 +0100

    remove files that have been superseded by skoruba implementation

--- src/Falc.Identity.Server/Models/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Duende Software. All rights reserved.
-// See LICENSE in the project root for license information.
-
-
-using Microsoft.AspNetCore.Identity;
-
-namespace Falc.Identity.Server.Models;
-
-public class ApplicationUser : IdentityUser;
